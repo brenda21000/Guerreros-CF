27ee9666b7eee50612506102bac27d9298786244
brenda <brendlores120@gmail.com>
2026-06-25 15:20:37 -0600

    Configurar WhiteNoise para Render

--- guerreros_proyecto/settings.py
+++ guerreros_proyecto/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 # ── MIDDLEWARE ─────────────────────────────────────────────────────────────
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,6 +86,8 @@ USE_TZ        = True
 STATIC_URL  = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'    # para collectstatic
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static_guerreros_cf",
